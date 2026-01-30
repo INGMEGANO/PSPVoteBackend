ad2b671a058124aa54945c24c06565ad0e547ac5
ingleandrocastro <146370389+ingleandrocastro@users.noreply.github.com>
2026-01-30 11:00:54 -0500

    ajustes de reporte a prod.....

--- src/utils/puppeteer.js
+++ src/utils/puppeteer.js
@@ -1,13 +1,25 @@
-import puppeteer from "puppeteer";
+import puppeteerCore from "puppeteer-core";
+import puppeteerFull from "puppeteer";
 
 export async function launchBrowser() {
-  return puppeteer.launch({
-    headless: true,
-    args: [
-      "--no-sandbox",
-      "--disable-setuid-sandbox",
-      "--disable-dev-shm-usage",
-      "--disable-gpu"
-    ]
+  const isProduction = process.env.NODE_ENV === "production";
+
+  if (isProduction) {
+    return puppeteerCore.launch({
+      headless: "new",
+      executablePath:
+        "/root/.cache/puppeteer/chrome/linux-144.0.7559.96/chrome-linux64/chrome",
+      args: [
+        "--no-sandbox",
+        "--disable-setuid-sandbox",
+        "--disable-dev-shm-usage",
+        "--disable-gpu"
+      ]
+    });
+  }
+
+  // Local
+  return puppeteerFull.launch({
+    headless: true
   });
 }
