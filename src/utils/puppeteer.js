e04b5695b72ac301d272bf2aa4bb5c811f88851b
ingleandrocastro <146370389+ingleandrocastro@users.noreply.github.com>
2026-01-30 10:53:02 -0500

    ajustes de reporte a prod....

--- src/utils/puppeteer.js
+++ src/utils/puppeteer.js
@@ -1,25 +1,13 @@
-import puppeteerCore from "puppeteer-core";
-import puppeteerFull from "puppeteer";
+import puppeteer from "puppeteer";
 
 export async function launchBrowser() {
-  const isProduction = process.env.NODE_ENV === "production";
-
-  if (isProduction) {
-    // 👉 EasyPanel / Docker
-    return puppeteerCore.launch({
-      headless: true,
-      executablePath: "/usr/bin/chromium",
-      args: [
-        "--no-sandbox",
-        "--disable-setuid-sandbox",
-        "--disable-dev-shm-usage",
-        "--disable-gpu"
-      ]
-    });
-  }
-
-  // 👉 Local (Windows / Mac / Linux)
-  return puppeteerFull.launch({
-    headless: true
+  return puppeteer.launch({
+    headless: true,
+    args: [
+      "--no-sandbox",
+      "--disable-setuid-sandbox",
+      "--disable-dev-shm-usage",
+      "--disable-gpu"
+    ]
   });
 }
