e6ec5c787e4378bb4b869dc4aa82f43c234087d4
ingleandrocastro <146370389+ingleandrocastro@users.noreply.github.com>
2026-02-03 17:13:49 -0500

    ajustes en validacion.

--- src/utils/puppeteer.js
+++ src/utils/puppeteer.js
@@ -1,14 +1,20 @@
 import puppeteerCore from "puppeteer-core";
 import puppeteerFull from "puppeteer";
+import fs from "fs";
 
 export async function launchBrowser() {
   const isProduction = process.env.NODE_ENV === "production";
 
   if (isProduction) {
+    const chromePath = "/usr/bin/google-chrome";
+
+    if (!fs.existsSync(chromePath)) {
+      throw new Error("Google Chrome no está instalado en el contenedor");
+    }
+
     return puppeteerCore.launch({
       headless: "new",
-      executablePath:
-        "/root/.cache/puppeteer/chrome/linux-144.0.7559.96/chrome-linux64/chrome",
+      executablePath: chromePath,
       args: [
         "--no-sandbox",
         "--disable-setuid-sandbox",
@@ -18,7 +24,7 @@ export async function launchBrowser() {
     });
   }
 
-  // Local
+  // Local (Windows / Linux / Mac)
   return puppeteerFull.launch({
     headless: true
   });
